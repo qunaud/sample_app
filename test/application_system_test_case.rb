9a1721f9365dc68f386d32a4f8013e695ac2722c
qunaud <qunaudo@gmail.com>
2022-07-23 20:12:21 +0900

    conect追加

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,18 +1,8 @@
 require "test_helper"
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1920, 1080]
 end
 
-test "should get home" do
-  get static_pages_home_url
-  assert_response :success
-end
-
-test "should get help" do
-  get static_pages_help_url
-  assert_response :success
-end
 end
