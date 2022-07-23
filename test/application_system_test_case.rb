f0671c5ed215246ad2f1bf423d2b7e3e6791a076
qunaud <qunaudo@gmail.com>
2022-07-23 18:44:35 +0900

    add static test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1920, 1080]
+end
+
+test "should get home" do
+  get static_pages_home_url
+  assert_response :success
+end
+
+test "should get help" do
+  get static_pages_help_url
+  assert_response :success
+end
 end
